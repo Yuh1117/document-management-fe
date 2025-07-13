d81f0d2020190dfc4392b65335e449bec72e5d10
Yuh <huypham3188@gmail.com>
2025-07-13 20:37:32 +0700

    multi lang & theme mode

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,9 +4,12 @@ import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
 import { useForm } from "react-hook-form"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { useState } from "react";
+import { useState, type ChangeEvent } from "react";
 import { Loader2Icon } from "lucide-react"
 import { FcGoogle } from "react-icons/fc";
+import { useTranslation } from "react-i18next";
+import { ChangeLanguage } from "@/components/settings/change-language";
+import { ModeToggle } from "@/components/settings/theme-toggle";
 
 const fieldNames: { [key: string]: string } = {
     email: "Email",
@@ -15,13 +18,15 @@ const fieldNames: { [key: string]: string } = {
 
 const Login = () => {
     const form = useForm();
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState<boolean>(false)
+    const { t } = useTranslation()
 
     const setError = (field: string, message: string): void => {
         form.setError(field, { type: "manual", message: message })
     }
 
     const validateEmpty = (field: string, value: string): boolean => {
+        form.clearErrors(field)
         if (!value) {
             setError(field, `${fieldNames[field]} không được để trống`);
             return false;
@@ -30,7 +35,6 @@ const Login = () => {
     }
 
     const validateEmail = (value: string): boolean => {
-        form.clearErrors('email')
         if (validateEmpty('email', value) === false) return false;
 
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
@@ -42,7 +46,6 @@ const Login = () => {
     }
 
     const validatePassword = (value: string): boolean => {
-        form.clearErrors('password')
         if (validateEmpty('password', value) === false) return false;
 
         return true
@@ -78,15 +81,15 @@ const Login = () => {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
 
-            <Card className="w-full md:max-w-md">
+            <Card className="w-full md:max-w-sm">
                 <CardContent >
                     <Form {...form}>
-                        <form className="p-2" onSubmit={form.handleSubmit(onSubmit)}>
+                        <form className="p-1" onSubmit={form.handleSubmit(onSubmit)}>
                             <div className="flex flex-col gap-6">
                                 <div className="flex flex-col items-center text-center gap-1">
-                                    <h1 className="text-2xl font-bold">DMS</h1>
+                                    <h1 className="text-2xl font-bold">{t('welcome')}</h1>
                                     <p className="text-muted-foreground text-balance">
-                                        Đăng nhập tài khoản của bạn
+                                        {t('login.title')}
                                     </p>
                                 </div>
 
@@ -97,9 +100,12 @@ const Login = () => {
                                         <FormItem>
                                             <FormLabel>Email</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="Nhập email" {...field}
+                                                <Input placeholder={t('login.email_placeholder')} {...field}
                                                     value={field.value || ""}
-                                                    onBlur={() => validateEmail(field.value)} />
+                                                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                                        form.setValue('email', e.target.value)
+                                                        validateEmail(e.target.value)
+                                                    }} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -111,11 +117,14 @@ const Login = () => {
                                     name="password"
                                     render={({ field }) => (
                                         <FormItem>
-                                            <FormLabel>Mật khẩu</FormLabel>
+                                            <FormLabel>{t('login.password')}</FormLabel>
                                             <FormControl>
-                                                <Input type="password" placeholder="Nhập mật khẩu" {...field}
+                                                <Input type="password" placeholder={t('login.password_placeholder')} {...field}
                                                     value={field.value || ""}
-                                                    onBlur={() => validatePassword(field.value)} />
+                                                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                                        form.setValue('password', e.target.value)
+                                                        validatePassword(e.target.value)
+                                                    }} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -123,31 +132,36 @@ const Login = () => {
                                 />
 
                                 <Button type="submit" className="w-full" disabled={loading}>
-                                    {loading ? <Loader2Icon className="animate-spin" /> : "Đăng nhập"}
+                                    {loading ? <Loader2Icon className="animate-spin" /> : t('login.label')}
                                 </Button>
 
                                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                                     <span className="bg-card text-muted-foreground relative z-10 px-2">
-                                        Hoặc tiếp tục với
+                                        {t('login.or_continue')}
                                     </span>
                                 </div>
 
                                 <div className="grid grid-cols-1 gap-4">
                                     <Button variant="outline" type="button" className="w-full">
                                         <FcGoogle />
-                                        Đăng nhập với Google
+                                        {t('login.google')}
                                     </Button>
                                 </div>
 
                                 <div className="text-center text-sm">
-                                    Chưa có tài khoản?{" "}
-                                    <Link to={"/signup"}><span className="underline underline-offset-4">Đăng ký</span></Link>
+                                    {t('login.no_account') + " "}
+                                    <Link to={"/signup"}><span className="underline underline-offset-4">{t('login.signup')}</span></Link>
                                 </div>
                             </div>
                         </form>
                     </Form>
                 </CardContent>
             </Card>
+
+            <div className="flex p-3 gap-2">
+                <ChangeLanguage />
+                <ModeToggle />
+            </div>
         </div>
     )
 }
