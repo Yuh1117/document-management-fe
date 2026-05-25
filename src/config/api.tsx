eae7953de577496269297403e0ddf1702a739d51
Yuh <huypham3188@gmail.com>
2026-05-25 22:59:45 +0700

    update api url

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "@/redux/store";
 import { setAccessToken, logout } from "@/redux/reducers/userSlice";
 
-const BASE_URL = 'http://localhost:8080/';
+const BASE_URL = import.meta.env.VITE_API_URL;
 const LANGUAGE_STORAGE_KEY = "language";
 
 export function getAcceptLanguage(): string {
